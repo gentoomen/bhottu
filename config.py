302abc4e2936d2f99e7bfeddea3cdc8c010e4930
tetsuo <anonuanon@googlemail.com>
2011-01-08 21:13:42 +0100

    accidentally removed config.py, uploading it again

---
+++ config.py
@@ -0,0 +1,21 @@
+#Config file for bhottu
+#filename: ./config.py
+
+#Connection options
+SERVER = 'irc.rizon.net'
+PORT = 6667
+NICK ='bhottu'
+REALNAME = 'bhottu'
+IDENT = 'bhottu'
+CHANNEL = ''
+MODE = 0 #This is a bitmask for user mode
+VHOST = False
+NICK_PASS = ''
+#Authed users
+GODS = ['']
+
+#Logging options
+RAW_LOGGING = True
+LOG_TO_STDOUT = True
+LOG_TO_FILE = True
+LOG_FILE = 'bhottu_log'
