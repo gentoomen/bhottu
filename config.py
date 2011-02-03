9eed1892da8e5586333b7aa1f630a3e7938a34fe
agaric <agaricster@gmail.com>
2011-02-04 01:09:16 +0200

    Fix a typo in config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@
 #Connection options
 SERVER = 'irc.rizon.net'
 PORT = 6667
-NICK ='SICPBot'
+NICK ='bhottu'
 REALNAME = 'bhottu'
 IDENT = 'bhottu'
-CHANNEL = '#/g/sicp'
+CHANNEL = ''
 MODE = 0 #This is a bitmask for user mode
 VHOST = True
-NICK_PASS = 'yH1#Ns1z7^F1'
+NICK_PASS = ''
 #Authed users
-GODS = ['agaric','chown','Manhose','imMute']
+GODS = []
 
 #Logging options
 RAW_LOGGING = True
