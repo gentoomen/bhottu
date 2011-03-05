23384c431200cad1f82442808c3f660d5dfdf1f4
Duke Derpington of Chesterfieldshire <poopsmith@lavabit.com>
2011-03-05 09:54:16 -0800

    adding temporary unescape() to outputTitle()

--- utils/utils.py
+++ utils/utils.py
@@ -46,3 +46,10 @@ def sendPM(unick, message):
 
 def authUser(unick):
     return unick in GODS
+
+def unescape(s):
+    s = s.replace("&lt;", "<")
+    s = s.replace("&gt;", ">")
+    # this has to be last:
+    s = s.replace("&amp;", "&")
+    return s
