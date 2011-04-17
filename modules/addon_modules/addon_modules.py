3046ae34e535cfad1a25642f029e241a8924579f
naosia <staff@volatile-creativity.com>
2011-04-18 00:09:30 +0200

    Minor change, testing self-update function

--- modules/addon_modules/addon_modules.py
+++ modules/addon_modules/addon_modules.py
@@ -1246,7 +1246,7 @@ def Roulette(parsed):
     if parsed['event'] == 'PRIVMSG':
         if parsed['event_msg'] == 'roulette':
             if random.randrange(0, 6) == 5:
-                return('KICK %s %s :%s \r\n' % (CHANNEL, parsed['event_nick'], 'YOU WIN!!!'))
+                return('KICK %s %s :%s \r\n' % (CHANNEL, parsed['event_nick'], 'CONGRATULATIONS, YOU WON THE GRAND PRIZE!'))
             else:
                 return sendMsg(None, "You get to live for now")
 """
