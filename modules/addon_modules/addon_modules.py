566511cdb822434d30bf48a7dd507f8f5ca87fda
agaric <agaricster@gmail.com>
2011-05-03 04:01:00 +0300

    Seriously wtf

--- modules/addon_modules/addon_modules.py
+++ modules/addon_modules/addon_modules.py
@@ -695,16 +695,18 @@ def Greeting(parsed):
         reply = dbQuery("SELECT greeting FROM greetings WHERE nick=%s", [name])
         if len(reply) > 0:
             print reply[0][0]
+            print sendMsg(name, reply[0][0])
             time.sleep(2)
-            return sendMsg(name, reply[0][0], False)
+            return sendMsg(name, reply[0][0])
     if parsed['event'] == 'NICK':
         if authUser(parsed['event_msg']) == True:
             name = parsed['event_msg']
             reply = dbQuery("SELECT greeting FROM greetings WHERE nick=%s", [name])
             if len(reply) > 0:
                 print reply[0][0]
+                print sendMsg(name, reply[0][0])
                 time.sleep(2)
-                return sendMsg(name, reply[0][0], False)
+                return sendMsg(name, reply[0][0])
 
 
 def Colors(parsed):
