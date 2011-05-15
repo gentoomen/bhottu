f0973f0267bd6c5bcf29b7c1bf967d501f280b6b
Chown <sunderphon+git@gmail.com>
2011-05-15 19:46:40 +0100

    Fixed statistical error

--- modules/addon_modules/addon_modules.py
+++ modules/addon_modules/addon_modules.py
@@ -1052,7 +1052,7 @@ def Poll(parsed):
 def Statistics(parsed):
     #funcs
     def top10Ever(parsed):
-        reply = dbQuery("SELECT DISTINCT name FROM `lines`")
+        reply = dbQuery("SELECT DISTINCT name FROM `lines` WHERE name<>`learningcode`")
         top10 = []
         for line in reply:
             count = dbQuery("SELECT COUNT(*) FROM `lines` WHERE name=%s",\
