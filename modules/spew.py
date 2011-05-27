8b3dcd8a5c6e1d6f7b638f9229f1b787b7b60cc3
Ruud Koolen <redlizard@redlizard.nl>
2011-05-27 03:38:01 +0200

    Ported the Spew module.

--- modules/spew.py
+++ modules/spew.py
@@ -1,50 +1,32 @@
-from config import *
-from utils import *
 from api import *
+import random
+import time
 
 def load():
-    registerParsedEventHandler(Spew)
     dbExecute('''create table if not exists `lines` (
               lineID int auto_increment primary key,
               name varchar(255),
               message text,
               time int,
               index(name) )''')
+    registerMessageHandler(None, recordMessage)
+    registerFunction("spew like %s", spew, "spew like <person>")
+    registerFunction("spew improv", spewImprov)
 registerModule('Spew', load)
 
-def Spew(parsed):
-    def intoLines(parsed):
-        if parsed['event'] == 'PRIVMSG':
-            message = parsed['event_msg']
-            nick = parsed['event_nick']
-            dbExecute("INSERT INTO `lines` (name, message, time) VALUES (%s, %s, %s)", [nick, message, int(time.time())])
+def recordMessage(channel, sender, message):
+    dbExecute("INSERT INTO `lines` (name, message, time) VALUES (%s, %s, %s)", (sender, message, int(time.time())))
 
-    def spewLines(parsed):
-        if parsed['event'] == 'PRIVMSG':
-            message = parsed['event_msg']
-            #nick = parsed['event_nick']
-            # never used
-            combostring = NICK + ", spew like "
-            if combostring in message:
-                name = message.replace(combostring, '')
-                name = name.strip()
-                reply = dbQuery("SELECT message FROM `lines` WHERE name=%s \
-                        ORDER BY RAND() LIMIT 1", [name])
-                for row in reply:
-                    sendMessage(CHANNEL, row[0])
-            elif message.startswith(NICK+', spew improv'):
-                branch = dbQuery("SELECT message FROM `lines` ORDER BY RAND() LIMIT 1")[0][0]
-                branch = random.choice(branch.split(random.choice\
-                (branch.split(' ')))).lstrip().rstrip()
-                limit = random.randint(2,4)
-                itercount = 0
-                while itercount < limit:
-                    stem = dbQuery("SELECT message FROM `lines` ORDER BY RAND() LIMIT 1")[0][0]
-                    root = random.choice(stem.split(' '))
-                    flower = random.choice(stem.split(root)).lstrip().rstrip()
-                    branch = branch + " " + flower
-                    itercount+=1
-                sendMessage(CHANNEL, branch)
+def spew(channel, sender, target):
+    """Repeats a random line a target person has ever said."""
+    result = dbQuery("SELECT message FROM `lines` WHERE name=%s ORDER BY RAND() LIMIT 1", [target])
+    if len(result) == 0:
+        return
+    sendMessage(channel, result[0][0])
 
-    intoLines(parsed)
-    spewLines(parsed)
+def spewImprov(channel):
+    """Builds a random "sentence" out of the chat history."""
+    parts = []
+    for (message,) in dbQuery("SELECT message FROM `lines` ORDER BY RAND() LIMIT %s" % random.randint(3, 5)):
+        parts.append(random.choice(message.split(random.choice(message.split(' ')))).strip())
+    sendMessage(channel, ' '.join(parts))
