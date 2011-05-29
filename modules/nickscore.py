2b9011217f8e372df5325f7df3ac9eabf3cbff48
Ruud Koolen <redlizard@redlizard.nl>
2011-05-30 00:02:06 +0200

    Ported the NickScore module.

--- modules/nickscore.py
+++ modules/nickscore.py
@@ -1,68 +1,59 @@
-from config import *
-from utils import *
 from api import *
-
 import re
 
 def load():
-    registerParsedEventHandler(nickPlus)
-    registerParsedEventHandler(queryNick)
-    dbExecute('''create table if not exists nickplus (
-              nickplusID int auto_increment primary key,
+    dbExecute('''create table if not exists nickscore (
+              nickscoreID int auto_increment primary key,
               name varchar(255),
               points int,
               unique(name) )''')
+    registerMessageHandler(None, searchNickPlus)
+    registerMessageHandler(None, searchNickMinus)
+    registerFunction("tell me about %s", tellMeAbout, "tell me about <target>")
+    registerFunction("show me the top %!i", showTop, "show me the top [amount]")	
 registerModule('NickScore', load)
 
-def nickPlus(parsed):
-    if parsed['event'] == 'PRIVMSG':
-        message = parsed['event_msg']
-        nick = parsed['event_nick']
-        uname = re.search('^\w+(?=\+{2})', message)
-        pointnum = None
-        if uname is not None:
-            uname = uname.group()
-            log.debug('message: %s' % message)
-            log.debug('nick: %s' % nick)
-            log.debug('uname: %s' % uname)
-            uname = uname.replace('++', '').rstrip()
-            if uname == nick:
-                sendMessage(nick, 'Plussing yourself is a little sad, is it not?')
-                return
-            uname = uname.replace('++', '')
-            try:
-                pointnum = int(dbQuery('SELECT points FROM nickplus WHERE name=%s', [uname])[0][0])
-            except:
-                log.error('Something went wrong!')
-            if pointnum is not None:
-                sendMessage(CHANNEL, 'incremented by one')
-                pointnum += 1
-                dbExecute('UPDATE nickplus set points=%s WHERE name=%s', [pointnum, uname])
-                log.info('Incremented by 1 %s' % uname)
-            elif pointnum == None:
-                sendMessage(CHANNEL, 'Added record')
-                dbExecute('INSERT INTO nickplus (name, points) VALUES (%s, %s)', [uname, 1])
-                log.info('nickPlus(): Incremented by 1 %s' % uname)
+def searchNickPlus(channel, sender, message):
+    match = re.match('\s*(\w+)\+\+$', message)
+    if match == None:
+        return
+    target = match.group(1)
+    if target == sender:
+        sendMessage(channel, "%s: Plussing yourself is a little sad, is it not?" % sender)
+        return
+    if len(dbQuery('SELECT points FROM nickscore WHERE name = %s', [target])) == 0:
+        dbExecute('INSERT INTO nickscore (name, points) VALUES (%s, 1)', [target])
+    else:
+        dbExecute('UPDATE nickscore SET points = points + 1 WHERE name = %s', [target])
+    sendMessage(channel, "Incremented by one")
 
+def searchNickMinus(channel, sender, message):
+    match = re.match('\s*(\w+)--$', message)
+    if match == None:
+        return
+    target = match.group(1)
+    if len(dbQuery('SELECT points FROM nickscore WHERE name = %s', [target])) == 0:
+        dbExecute('INSERT INTO nickscore (name, points) VALUES (%s, -1)', [target])
+    else:
+        dbExecute('UPDATE nickscore SET points = points - 1 WHERE name = %s', [target])
+    sendMessage(channel, "Decremented by one")
 
-def queryNick(parsed):
-    if parsed['event'] == 'PRIVMSG':
-        message = parsed['event_msg']
-        nick = parsed['event_nick']
-        combostring = NICK + ", tell me about "
-        if combostring in message:
-            uname = message.split(combostring)[1].replace('++', '')
-            log.debug('Querying DB with: %s' % uname)
-            try:
-                pointnum = int(dbQuery('SELECT points FROM nickplus WHERE name=%s', [uname])[0][0])
-                sendMessage(CHANNEL, '%s, Points for %s = %s' % (nick, uname, pointnum))
-            except:
-                pass
-
+def tellMeAbout(channel, sender, target):
+    result = dbQuery('SELECT points FROM nickscore WHERE name = %s', [target])
+    if len(result) == 0:
+        score = 0
+    else:
+        score = result[0][0]
+    sendMessage(channel, "%s, %s has a score of %s points" % (sender, target, score))
 
 def showTop(channel, sender, amount):
-    registerFunction("show me the top %!i", showtop)
-    for (name, points) in dbQuery("SELECT name, points FROM nickplus ORDER BY points LIMIT %i"
-    % amount):
-        toplist = toplist + "%s (%d) " % (name, points)
-    sendMessage(channel, toplist)
+    if amount == None:
+        amount = 10
+    if amount < 0:
+        return
+    summary = ''
+    index = 1
+    for (name, points) in dbQuery('SELECT name, points FROM nickscore ORDER BY points DESC LIMIT %i' % amount):
+        summary += "%i. %s [%i] " % (index, name, points)
+        index += 1
+    sendMessage(channel, summary.strip())
