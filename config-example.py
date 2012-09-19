9045523fce2dfb6c04c760ba6eb1743b1db1b7a5
copypaste <copypaste@bitlegend.com>
2012-09-19 17:57:34 -0400

    Configuration example

---
+++ config-example.py
@@ -0,0 +1,57 @@
+#___.   .__            __    __         
+#\_ |__ |  |__   _____/  |__/  |_ __ __ 
+# | __ \|  |  \ /  _ \   __\   __|  |  \
+# | \_\ |   Y  (  <_> |  |  |  | |  |  /
+# |___  |___|  /\____/|__|  |__| |____/ 
+#     \/     \/                         
+#
+#
+SERVER = "irc.rizon.net" #server you wish for the bot to connect to
+PORT = 6667 #note that the bot does not yet support SSL
+NICK = "bhottu" #nick you want the bot to use
+IDENT = "bot" #ident you want the bot to send to the IRC server. note this value will be ignored if you have an identd installed
+MODE = 0 #numerical IRC mode. see section 3.1.3 of RFC2812. Common values are "0" and "8" (invisible)
+REALNAME = "bot" #real name sent to the server. Displayed in /whois
+CHANNEL = "" #channel you want the bot to join on start
+NICK_PASS = None #enter a string here to have the bot auto-auth to nickserv
+
+#Logging information
+STDOUT_LOGLEVEL = "DEBUG"
+STDOUT_VERBOSE = True
+
+LOG_FILE = "log"
+LOG_LEVEL = "INFO"
+LOG_VERBOSE = True
+
+#These users are allowed to use all privileged commands regardless of if they're loaded in the Admin module or not
+GODS = [""]
+
+#See the section in README.md for more information 
+DB_HOSTNAME = "localhost"
+DB_USERNAME = "root"
+DB_PASSWORD = ""
+DB_DATABASE = "bhottu"
+
+#recommended modules. For more, browse modules/. There is documentation about writing them in the README and the default ones serve as good examples
+ENABLED_MODULES = [
+    'Quit',
+    'Admins',
+    'AutoUpdate',
+    'FloodControl',
+   
+    'NickScore',
+    'LinkTitle',
+    'Quotes',
+    'Reply',
+    'Spew',
+    'Greetings',
+    'Colors',
+    'Poll',
+    'Statistics',
+    'LoadAverage',
+    'Admins',
+    'Ignore',
+    'Echo',
+    'Help',
+    'Weather',
+]
