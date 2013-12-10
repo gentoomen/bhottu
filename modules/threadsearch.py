307af3d7de649636e4c01cf681c088127701b374
codyharrington <thecodeman66@hotmail.com>
2013-12-11 02:13:40 +1300

    Completely overhauled threadsearch to use threading and a synchronised queue now. Also, when searching for catalog, a new thread will be spawned per page. When searching through the whole board, the threads.json link is used instead of catalog.json, and a new python thread is spawned per 4chan thread on a page

--- modules/threadsearch.py
+++ modules/threadsearch.py
@@ -11,7 +11,8 @@ from api import *
 from time import sleep
 import re
 import math
-from multiprocessing import *
+from threading import *
+from Queue import *
 import time
 
 def load():
@@ -34,41 +35,25 @@ def output_to_sprunge(string):
     response.close()
     return message
 
-def run_catalog_search(channel, sender, board, string, all_posts=False):
-    """Search 4chan posts for a search string and return
-    the matching post numbers"""
-    # Strip unyieldly characters
+def process_results(channel, sender, board, string, posts):
+    """Process the resulting data of a search and present it"""
+    max_num_urls_displayed = 3
     board = sanitise(board)
     message = ""
-    try:
-        posts = search_catalog(string, board, all_posts)
-        posts = list(set(posts))
-    except Exception as e:
-        log.error(e)
-        raise
     if len(posts) <= 0:
         sendMessage(channel, "{0}: No results for {1}".format(sender, string))
     else:
-        # Turn the post numbers into fully-fledged URLs
         post_template = "https://boards.4chan.org/{0}/res/{1}"
         urls = [post_template.format(board, post_num) for post_num in posts]
-        if len(urls) > 3:
-            # If the number of posts is more than 3,
-            # then send it to sprunge.us
+        if len(urls) > max_num_urls_displayed:
             message = output_to_sprunge('\n'.join(urls))
         else:
-            message = " ".join(urls[:3])
+            message = " ".join(urls[:max_num_urls_displayed])
         sendMessage(channel, "{0}: {1}".format(sender, message))
 
-def run_board_search(channel, sender, board, string):
-    """Wrapper for catalog search to make it search
-    the entire board"""
-    run_catalog_search(channel, sender, board, string, True)
-
-def get_json_data(url, sleep_time=1):
+def get_json_data(url, sleep_time=0):
     """Returns a json data object from a given url."""
     # Respect 4chan's rule of at most 1 JSON request per second
-    # but only if searching the catalog
     sleep(sleep_time)
     try:
         open_url = urllib2.urlopen(url)
@@ -76,7 +61,6 @@ def get_json_data(url, sleep_time=1):
         open_url.close()
         return json_data
     except urllib2.HTTPError as e:
-        # Handle if a thread has 404'd
         if e.getcode() == 404:
             pass
         else:
@@ -85,73 +69,80 @@ def get_json_data(url, sleep_time=1):
         log.error(e)
         raise
 
-def search_thread(mutex, thread_num, process_data, found_list):
+def search_thread(mutex, results_queue, thread_num, search_specifics):
     """
     Searches every post in thread thread_num on board board for the
     string provided. Returns a list of matching post numbers.
     """
-    json_url = "https://api.4chan.org/{0}/res/{1}.json".format(process_data["board"], thread_num)
-    thread_json = get_json_data(json_url, process_data["request_delay"])
+    json_url = "https://a.4cdn.org/{0}/res/{1}.json".format(search_specifics["board"], thread_num)
+    thread_json = get_json_data(json_url)
     re_search = None
     for post in thread_json["posts"]:
-        user_text = "".join([post[s] for s in process_data["sections"] if s in post.keys()])
-        re_search = re.search(process_data["string"], user_text, re.UNICODE)
+        user_text = "".join([post[s] for s in search_specifics["sections"] if s in post.keys()])
+        re_search = re.search(search_specifics["string"], user_text, re.UNICODE)
         if re_search is not None:
             mutex.acquire()
             found_list.append("{0}#p{1}".format(thread_num, post["no"]))
             mutex.release()
 
-def search_page(mutex, process_count, page, process_data, found_list):
-    """Will be run by the multiprocessing module. Searches all the 
-    threads on a page and stores any matching results in an array"""
-    # Process count is used as a counting semaphore to 
-    # keep track of how many processes are currently active
-    process_count.value += 1
+def search_page(mutex, results_queue, page, search_specifics):
+    """Will be run by the threading module. Searches all the 
+    4chan threads on a page and adds matching results to synchronised queue"""
     for thread in page['threads']:
-        user_text = "".join([thread[s] for s in process_data["sections"] if s in thread.keys()])
-        if re.search(process_data["string"], user_text, re.UNICODE) is not None:
+        user_text = "".join([thread[s] for s in search_specifics["sections"] if s in thread.keys()])
+        if re.search(search_specifics["string"], user_text, re.UNICODE) is not None:
             mutex.acquire()
-            found_list.append(thread["no"])
+            results_queue.put(thread["no"])
             mutex.release()
-        if process_data["all_posts"]:
-            search_thread(mutex, thread['no'], process_data, found_list)
-    process_count.value -= 1
-
-def search_catalog(string, board, all_posts):
-    """
-    Searches through every thread on a board. Sets the sleep limit as 
-    the base 2 logarithm of    how many posts there are on a board
-    """
-    manager = Manager()
-    mutex = manager.Lock()
-    process_count = Value('i', 0)
-    json_url = "https://api.4chan.org/{0}/catalog.json".format(board)
+        
+def search_catalog(channel, sender, board, string):
+    """Search all OP posts on the catalog of a board, and return matching results"""
+    results_queue = Queue()
+    mutex = RLock()
+    json_url = "https://a.4cdn.org/{0}/catalog.json".format(board)
     sections = ["com", "name", "trip", "email", "sub", "filename"]
-    found_list = manager.list()
     catalog_json = get_json_data(json_url)
-    # Add in an artificial delay of the log base 2 of the number of posts on that board.
-    # Moot requests that there be at least 1 second between 4chan API requests
-    # This will create at least some form of delay
-    #thread_count = sum([len(page['threads']) for page in catalog_json])
-    #request_delay = float(math.log(thread_count, 2)) / thread_count
-    request_delay = 0
-    process_data = {"sections" : sections, "request_delay" : request_delay,
-            "board" : board, "string" : string, "all_posts" : all_posts} 
-    timeout_value = 60
+    search_specifics = {"sections" : sections, "board" : board, "string" : string}
+    thread_pool = []
 
     for page in catalog_json:
-        p = Process(target=search_page, args=(mutex, process_count, page, process_data, found_list))
-        # Make sure that the child process is teriminated once this parent process is
-        p.daemon = True
-        p.start()
+        t = Thread(None, target=search_page, args=(mutex, results_queue, page, search_specifics, search_all_threads))
+        t.start()
+        thread_pool.append(t)
+
+    for _thread in thread_pool:
+        if _thread.is_alive():
+            _thread.join()
     
-    timeout_timer = time.clock()
-    # Wait until all processes have completed
-    while process_count.value > 0:
-        # Have a timer running in case one of the child processes doesn't finish 
-        if (time.clock() - timeout_timer) > timeout_value:
-            log.error("Timed out")
-            break
-    return found_list
+    process_results(channel, sender, board, string, posts)
+
+def search_board(channel, sender, board, string):
+    """Search all the posts on a board and return matching results"""
+    results_queue = Queue()
+    mutex = RLock()
+    json_url = "https://a.4cdn.org/{0}/threads.json".format(board)
+    sections = ["com", "name", "trip", "email", "sub", "filename"]
+    threads_json = get_json_data(json_url)
+    search_specifics = {"sections" : sections, "board" : board, "string" : string}
+    thread_pool = []
+
+    for page in threads_json:
+        for thread in page["threads"]:
+            t = Thread(None, target=search_thread, args=(mutex, results_queue, thread["no"], search_specifics))
+            t.start()
+            thread_pool.append(t)
+    
+    for _thread in thread_pool:
+        if _thread.is_alive():
+            _thread.join()
+
+    process_results(channel, sender, board, string, posts)
+
+
+
+
+
+
+
 
 
