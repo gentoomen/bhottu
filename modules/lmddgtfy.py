5bce110e4948410ed924861c502c59fcf80a7760
undeadmastodon <jonasmo441@gmail.com>
2014-03-05 00:43:11 +0100

    Improved lmddgtfy module

--- modules/lmddgtfy.py
+++ modules/lmddgtfy.py
@@ -1,20 +1,26 @@
 from api import *
 import re
 
-LINK = "Here m8, http://www.lmddgtfy.net/?q={}"
+LINK = "Here m8, http://www.duckduckgo.com/?q={}"
 NOSEARCH = [
         ## Might just make this a table instead
         "up",
         "going",
         "happening",
         "with",
-        "all",
         "so",
-        "the",
         "very",
         "a",
         "that",
+        "it",
+        "your",
 ]
+
+BINDINGS = [
+        "a"
+        ]
+
+NOT_ALLOWED = set("?,.")
 
 def load():
     """ Searches terms for users on duckduckgo (with lmddgtfy) """
@@ -22,10 +28,14 @@ def load():
 registerModule('Lmddgtfy', load)
 
 def lmstfy(channel, sender, message):
-    match = re.search(r"what(?:'| i)?s (\w+)", message, re.IGNORECASE)
+    match = re.search(r"^what(?:'| i)?s (.+)", message, re.IGNORECASE)
     if match:
-        word = match.groups()[-1]
-        if word in NOSEARCH:
-            return
-        sendMessage(channel, LINK.format(word))
+        usernames = channelUserList(channel)
+        term = "".join(
+                [char for char in match.groups()[0] if char not in NOT_ALLOWED]).split(" ")
+        if term[0] in BINDINGS:
+            term = term[1:]
+        term = "%20".join(
+                [word for word in term if word not in usernames])
+        sendMessage(channel, LINK.format(term))
 
