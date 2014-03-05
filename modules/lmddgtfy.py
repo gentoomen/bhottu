79c1d67b17d3e9bdcc6e9cba415b26eea04dc8d9
undeadmastodon <jonasmo441@gmail.com>
2014-03-05 02:07:45 +0100

    Fixed the splitting of words in lmddgtfy.py

--- modules/lmddgtfy.py
+++ modules/lmddgtfy.py
@@ -2,7 +2,7 @@ from api import *
 import re
 
 LINK = "Here m8, http://www.duckduckgo.com/?q={}"
-NOSEARCH = [
+NOSEARCH = set([
         ## Might just make this a table instead
         "up",
         "going",
@@ -14,11 +14,12 @@ NOSEARCH = [
         "that",
         "it",
         "your",
-]
+])
 
-BINDINGS = [
-        "a"
-        ]
+BINDINGS = set([
+        "a",
+        "the",
+        ])
 
 NOT_ALLOWED = set("?,.")
 
@@ -28,14 +29,24 @@ def load():
 registerModule('Lmddgtfy', load)
 
 def lmstfy(channel, sender, message):
+    if any(x for x in message if x == "\0"):
+        sendMessage(channel, "Fak u {}".format(sender))
+        return
+
     match = re.search(r"^what(?:'| i)?s (.+)", message, re.IGNORECASE)
     if match:
         usernames = channelUserList(channel)
         term = "".join(
-                [char for char in match.groups()[0] if char not in NOT_ALLOWED]).split(" ")
+                [char for char in match.groups()[0] if char not in NOT_ALLOWED])
+        term = re.split(r"\s+", term)
         if term[0] in BINDINGS:
             term = term[1:]
+        if '' in term:
+            return
         term = "%20".join(
                 [word for word in term if word not in usernames])
+        if not term:
+            sendMessage(channel, "Fak u {}".format(sender))
+            return
         sendMessage(channel, LINK.format(term))
 
