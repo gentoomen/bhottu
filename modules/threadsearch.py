e62776a77f1109e1ed3939379340a422915c3f04
Cody Harrington <thecodeman66@hotmail.com>
2014-05-13 02:31:52 +1200

    Did some code refactoring as well as handling non-existent boards.

--- modules/threadsearch.py
+++ modules/threadsearch.py
@@ -5,7 +5,6 @@ url of any matching post
 :author Cody Harrington:
 """
 import re
-import math
 import json
 import time
 import requests
@@ -18,33 +17,46 @@ from collections import deque
 
 def load():
     """Load the module"""
-    registerFunction("catalog %s %S", search_catalog, "catalog <board> <regex>")
-    registerFunction("board %s %S", search_board, "board <board> <regex>")
+    registerFunction("catalog %s %S", catalog_search_handler, "catalog <board> <regex>")
+    registerFunction("board %s %S", board_search_handler, "board <board> <regex>")
 registerModule("ThreadSearch", load)
 
 def sanitise(string):
     """Strips a string of all non-alphanumeric characters"""
     return re.sub(r"[^a-zA-Z0-9 ]", "", string)
 
-def process_results(channel, sender, board, string, results_deque):
+def catalog_search_handler(channel, sender, board, user_regex):
+    """Handler for initiating catalog search"""
+    board = sanitise(board)
+    results_data = perform_concurrent_4chan_search(board, user_regex, catalog_search=True)
+    process_results(channel, sender, results_data)
+
+def board_search_handler(channel, sender, board, user_regex):
+    """Handler for initiating full board search"""
+    board = sanitise(board)
+    results_data = perform_concurrent_4chan_search(board, user_regex, catalog_search=False)
+    process_results(channel, sender, results_data)
+
+def process_results(channel, sender, results_data):
     """Process the resulting data of a search and present it"""
     max_num_urls_displayed = 3
-    board = sanitise(board)
-    message = ""
-    if len(results_deque) <= 0:
-        sendMessage(channel, "{0}: No results for {1}".format(sender, string))
+    search_parameters = results_data["search_parameters"]
+    post_numbers = results_data["post_numbers"]
+
+    if len(post_numbers) <= 0:
+        sendMessage(channel, "{0}: No results for {1} on that board".format(sender, search_parameters["string"],
+                                                                  search_parameters["board"]))
     else:
         post_template = "https://boards.4chan.org/{0}/thread/{1}"
-        urls = [post_template.format(board, post_num) for post_num in results_deque]
+        urls = [post_template.format(search_parameters["board"], post_num) for post_num in post_numbers]
         if len(urls) > max_num_urls_displayed:
             message = nnmm('\n'.join(urls))
         else:
             message = " ".join(urls[:max_num_urls_displayed])
         sendMessage(channel, "{0}: {1}".format(sender, message))
 
-def get_json_data(url, sleep_time=0):
+def get_json_data(url):
     """Returns a json data object from a given url."""
-    sleep(sleep_time)
     try:
         response = requests.get(url)
         if response.status_code == 404:
@@ -54,80 +66,67 @@ def get_json_data(url, sleep_time=0):
         return json_data
     except Exception as e:
         exception_string = "url: {0} status_code: {1}\n{2}".format(url,
-                response.status_code, traceback.format_exec)
+                response.status_code, traceback.format_exec())
         log.error(exception_string)
         print(exception_string)
         raise
 
-def search_thread(results_deque, thread_num, search_specifics):
+def search_thread(results_deque, thread_num, search_parameters):
     """
-    Searches every post in thread thread_num on board board for the
+    Searches every post in thread thread_num on a board for the
     string provided. Returns a list of matching post numbers.
     """
-    json_url = "https://a.4cdn.org/{0}/thread/{1}.json".format(search_specifics["board"], thread_num)
+    json_url = "https://a.4cdn.org/{0}/thread/{1}.json".format(search_parameters["board"], thread_num)
     thread_json = get_json_data(json_url)
     if thread_json is None:
         return
 
-    regex_match = search_specifics["compiled_regex"].match
+    regex_match = search_parameters["compiled_regex"].match
     for post in thread_json["posts"]:
-        user_text = "".join([post[s] for s in search_specifics["sections"] if s in post])
+        user_text = "".join([post[s] for s in search_parameters["sections"] if s in post])
         if regex_match(user_text) is not None:
             results_deque.append("{0}#p{1}".format(thread_num, post["no"]))
 
-def search_page(results_deque, page, search_specifics):
+def search_catalog_page(results_deque, page, search_parameters):
     """Will be run by the threading module. Searches all the 
     4chan threads on a page and adds matching results to synchronised queue"""
-    regex_match = search_specifics["compiled_regex"].match
-    for thread in page['threads']:
-        user_text = "".join([thread[s] for s in search_specifics["sections"] if s in thread])
+    regex_match = search_parameters["compiled_regex"].match
+    for thread in page["threads"]:
+        user_text = "".join([thread[s] for s in search_parameters["sections"] if s in thread])
         if regex_match(user_text) is not None:
             results_deque.append(thread["no"])
-        
-def search_catalog(channel, sender, board, string):
-    """Search all OP posts on the catalog of a board, and return matching results"""
+
+def perform_concurrent_4chan_search(board, user_regex, catalog_search=False):
+    """Search a thread or catalog on 4chan using several threads concurrently, then return relevant data"""
     thread_join_timeout_seconds = 10
     results_deque = deque()
-    json_url = "https://a.4cdn.org/{0}/catalog.json".format(board)
+    json_url = "https://a.4cdn.org/{0}/{1}.json".format(board, "catalog" if catalog_search else "threads")
     sections = ["com", "name", "trip", "email", "sub", "filename"]
-    catalog_json = get_json_data(json_url)
-    search_regex = re.compile(string, re.UNICODE + re.IGNORECASE)
-    search_specifics = {"sections": sections, "board": board, "string": string,
+    json_data = get_json_data(json_url)
+    search_regex = re.compile(user_regex, re.UNICODE + re.IGNORECASE)
+    search_parameters = {"sections": sections, "board": board, "string": user_regex,
             "compiled_regex": search_regex}
+    results_data = {"post_numbers": results_deque, "search_parameters": search_parameters}
     thread_pool = []
 
-    for page in catalog_json:
-        t = Thread(None, target=search_page, args=(results_deque, page, search_specifics))
-        t.start()
-        thread_pool.append(t)
+    if json_data is None:
+        return results_data
 
-    for _thread in thread_pool:
-        if _thread.is_alive():
-            _thread.join(float(thread_join_timeout_seconds))
-
-    process_results(channel, sender, board, string, results_deque)
-
-def search_board(channel, sender, board, string):
-    """Search all the posts on a board and return matching results"""
-    thread_join_timeout_seconds = 10
-    results_deque = deque()
-    json_url = "https://a.4cdn.org/{0}/threads.json".format(board)
-    sections = ["com", "name", "trip", "email", "sub", "filename"]
-    threads_json = get_json_data(json_url)
-    search_regex = re.compile(string, re.UNICODE + re.IGNORECASE)
-    search_specifics = {"sections": sections, "board": board, "string": string,
-            "compiled_regex": search_regex}
-    thread_pool = []
-
-    for page in threads_json:
-        for thread in page["threads"]:
-            t = Thread(None, target=search_thread, args=(results_deque, thread["no"], search_specifics))
+    for page in json_data:
+        if catalog_search:
+            t = Thread(None, target=search_catalog_page, args=(results_deque, page, search_parameters))
             t.start()
             thread_pool.append(t)
-    
+        else:
+            for thread in page["threads"]:
+                t = Thread(None, target=search_thread, args=(results_deque, thread["no"], search_parameters))
+                t.start()
+                thread_pool.append(t)
+
     for _thread in thread_pool:
         if _thread.is_alive():
             _thread.join(float(thread_join_timeout_seconds))
 
-    process_results(channel, sender, board, string, results_deque)
+    return results_data
+
 
