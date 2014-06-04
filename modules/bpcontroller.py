b907bd79a41fa1b8c916e50602c445cf90180913
Chown <sunderphon@gmail.com>
2014-06-04 23:58:36 +0100

    Created a module for controlling unruly users

---
+++ modules/bpcontroller.py
@@ -0,0 +1,26 @@
+from api import *
+
+def load():
+    """Restricts spamming annoyed SOPs"""
+    dbExecute('''create table if not exists bpname (
+        currentname varchar(255);
+        UPDATE bpname SET currentname = 'bradpitt'
+        )''')
+    registerMessageHandler(None, BPControl)
+    registerMessageHandler("BP changed name to %S", BPNameChange, restricted=True)
+
+registerModule("BPController", load)
+
+def BPControl(channel, sender, message):
+    BPName = dbQuery('SELECT currentname FROM bpname')
+    if len(BPName) = 0:
+        sendprivmg
+    if sender.lower() == BPName[0].lower() 
+        if "chown" in message or "sell" in message:
+            sendKick(channel, sender, "Wasn't ever funny")
+    else:
+        return
+
+def BPNameChange(sender, message):
+    result = dbExecute('UPDATE bpname SET currentname = %s', message)
+    sendMessage(channel, "BP's name has been updated. Thanks for fighting the good fight.")
