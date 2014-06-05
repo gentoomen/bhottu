0bc10150500957b9737a21325ce4e672b40f917d
Chown <sunderphon@gmail.com>
2014-06-06 00:19:47 +0100

    This time, no SQL

--- modules/bpcontroller.py
+++ modules/bpcontroller.py
@@ -1,24 +1,19 @@
 from api import *
 
+_currentName = "bradpitt"
+
 def load():
     """Restricts spamming annoyed SOPs"""
-    dbExecute('''create table if not exists bpname (
-        currentname varchar(255);
-        UPDATE bpname SET currentname = 'bradpitt'
-        )''')
     registerMessageHandler(None, BPControl)
     registerMessageHandler("BP changed name to %S", BPNameChange, restricted=True)
 
 registerModule("BPController", load)
 
 def BPControl(channel, sender, message):
-    BPName = dbQuery('SELECT currentname FROM bpname')
-    if len(BPName) == 0:
-        sendPrivmsg("chown", "NO NAME SET") # So I don't annoy other people
-    if sender.lower() == BPName[0].lower():
+    if sender.lower() == _currentName.lower():
         if "chown" in message or "sell" in message:
             sendKick(channel, sender, "Wasn't ever funny")
             
 def BPNameChange(sender, message):
-    result = dbExecute('UPDATE bpname SET currentname = %s', message)
+    _currentName = message
     sendMessage(channel, "BP's name has been updated. Thanks for fighting the good fight.")
